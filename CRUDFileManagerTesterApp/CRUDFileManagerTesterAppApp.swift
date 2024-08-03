99f07a8e53f6347ff1285669d2357501fe75da2c
Kavinda Dilshan <kdtechniques@gmail.com>
2024-08-03 18:53:05 +0530

    Initial Commit

---
+++ CRUDFileManagerTesterApp/CRUDFileManagerTesterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUDFileManagerTesterAppApp.swift
+//  CRUDFileManagerTesterApp
+//
+//  Created by Mr. Kavinda Dilshan on 2024-08-03.
+//
+
+import SwiftUI
+
+@main
+struct CRUDFileManagerTesterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CRUDFileManagerTesterApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CRUDFileManagerTesterApp
+//
+//  Created by Mr. Kavinda Dilshan on 2024-08-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
